7a10634f9c40ed0ec0f7d3237466abe70ea71e06
rick019 <87342919+rick019@users.noreply.github.com>
2021-10-07 14:08:55 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-function CarregarFilmes(){
-//array --> variaveis com subdivisões (índices)
-    let filmes = ["images/chegada.png" , "images/yesterday.png" , "images/escola.png" , "images/round6.jpg" , "images/fabricadechocolate.jpg" , "images/hunter.jpg"] 
+    //array --> variaveis com subdivisões (índices)
+    let filmes = ["images/chegada.png", "images/yesterday.png", "images/escola.png", "images/round6.jpg"]
 
+//refatoração
+//o que é?: melhorar um codigo existente.
+function CarregarFilmes() {
+let totalFilmes = filmes.length
+let divListaFilmes = document.querySelector(".lista-filmes")
 
+//limpando a carregar filmes, pra depois inserir as coisas dentro ( para nao ficar repetindo todas as imagens que ja tinham, enquanto adiciona outra)
+    divListaFilmes.innerHTML = ""
     //laço de repetição
-    for(let controle = 0 ; controle < 6 ; controle = controle + 1){
-        document.querySelector(".lista-filmes").innerHTML += "<img src=" + filmes [controle] + ">"
+    for (let i = 0; i < totalFilmes ; i++) {
+        divListaFilmes.innerHTML += "<img src=" + filmes[i] + ">"
     }
+}
+
+function AdicionarFilme(){
+    let nomeFilme = document.querySelector("#nome-do-filme").value
+    console.log(nomeFilme)
+    let caminhoCompleto = "images/" + nomeFilme
+
+    filmes.push(caminhoCompleto)
+
+    CarregarFilmes()
 }
